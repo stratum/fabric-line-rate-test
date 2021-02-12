20bd241c7e7a2e59fa94a8fbe0574c0e97be6e71
Suneet Kumar Singh <38260494+ersuneetsingh@users.noreply.github.com>
2021-02-12 16:51:25 -0300

    Test cases to verify port shaping rate and priority scheduling  (#14)
    * In this PR, the FLiRT test case is added to verify the port shaping rate. Also, the chassis_config is added to support port shapign rate.
    
    * In this PR, the FLiRT test case is added to verify the port shaping rate. Also, the chassis_config is added to support port shapign rate.
    
    * In this PR, the FLiRT test case is added to verify priority scheduling.
    
    * add license
    
    * add assert statement and clean
    
    * fix the class name
    
    * fix mult
    
    * fix constants and vaiables representation
    
    * add assert statement
    
    * fix the indent and max burst bytes
    
    * assure flow priortization by adding one more delay critical flow without prioritization.
    
    * fix the representation and comments, and clean
    
    * Update stratum/chassis_config.pb_with_PortShaping.txt
    
    Co-authored-by: Yi Tseng <a86487817@gmail.com>
    
    * fix some minor representations and notations
    
    * fix error
    
    * fix and place license text inline at the top of the chassis config; remove the extra file
    
    * fix the file name.
    
    * fix the file name.
    
    Co-authored-by: Yi Tseng <a86487817@gmail.com>

---
+++ trex-scripts/tests/port_shaping_rate.py
@@ -0,0 +1,70 @@
+# SPDX-FileCopyrightText: Copyright 2020-present Open Networking Foundation.
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import os
+import json
+from argparse import ArgumentParser
+from datetime import datetime
+from scapy.layers.all import IP, TCP, UDP, Ether
+
+from lib.base_test import StatelessTest
+from lib.utils import list_port_status,get_readable_port_stats
+from lib.xnt import analysis_report_pcap
+from trex_stl_lib.api import STLPktBuilder, STLStream, STLTXCont, STLTXSingleBurst
+
+SOURCE_MAC = "00:00:00:00:00:01"
+DEST_MAC   = "00:00:00:00:00:02"
+
+
+SENDER_PORT   = [0]
+RECEIVER_PORT = [1]
+SHAPING_RATE_MBPS = 1000 # in Mbps
+
+
+class PortShapingSTL(StatelessTest):
+
+    # setup_subparser is an optional class method
+    # You can implement this method if you want to add additional command line
+    # parameters for your test.
+    # Those parameters will be parsed and be passed to the "start" method below as
+    # "args" argument.
+    @classmethod
+    def setup_subparser(cls, parser: ArgumentParser) -> None:
+        parser.add_argument("--duration", type=float, help="Test duration", default=10)
+
+
+    # The entrypoint of a test
+    def start(self, args: dict) -> None:
+
+        pkt = Ether(dst=DEST_MAC) / IP() / TCP() / ("*" * 1500)
+        # Create a traffic stream
+        stream = STLStream(packet=STLPktBuilder(pkt=pkt, vm=[]), mode=STLTXCont())
+        self.client.add_streams(stream, ports=[0])
+
+
+        logging.info(
+                "Starting traffic, duration: %d sec", args.duration,
+        )
+
+        # Start sending traffic
+        self.client.start(SENDER_PORT, mult="100%", duration=args.duration)
+
+        logging.info("Waiting until all traffic stop")
+        self.client.wait_on_traffic(ports=SENDER_PORT)
+
+        # Get statistics for TX and RX ports
+        stats = self.client.get_stats()
+        rx_rate_mbps = stats[1]['rx_bps'] / (10**6)
+        assert (SHAPING_RATE_MBPS*0.95 < rx_rate_mbps < SHAPING_RATE_MBPS), \
+        "The measured RX rate is not close to the port shaping rate"
+
+
+        readable_stats_0 = get_readable_port_stats(stats[0])
+        readable_stats_1 = get_readable_port_stats(stats[1])
+
+        print ("\n Statistics for TX port: \n")
+        print (readable_stats_0)
+
+        print ("\n Statistics for RX port: \n")
+        print (readable_stats_1)
