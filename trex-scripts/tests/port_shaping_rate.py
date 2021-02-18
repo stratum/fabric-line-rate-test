68cfbeca08253989e64f9b164643111c031c6153
Yi-Tseng <Yi-Tseng@users.noreply.github.com>
2021-02-18 07:49:00 +0000

    Auto-prettify 73a44b4e4976080b4e33051e957b9242b92a6e1c

--- trex-scripts/tests/port_shaping_rate.py
+++ trex-scripts/tests/port_shaping_rate.py
@@ -1,25 +1,25 @@
 # SPDX-FileCopyrightText: Copyright 2020-present Open Networking Foundation.
 # SPDX-License-Identifier: Apache-2.0
 
+import json
 import logging
 import os
-import json
 from argparse import ArgumentParser
 from datetime import datetime
-from scapy.layers.all import IP, TCP, UDP, Ether
 
 from lib.base_test import StatelessTest
-from lib.utils import list_port_status,get_readable_port_stats
+from lib.utils import get_readable_port_stats, list_port_status
 from lib.xnt import analysis_report_pcap
+from scapy.layers.all import IP, TCP, UDP, Ether
 from trex_stl_lib.api import STLPktBuilder, STLStream, STLTXCont, STLTXSingleBurst
 
 SOURCE_MAC = "00:00:00:00:00:01"
-DEST_MAC   = "00:00:00:00:00:02"
+DEST_MAC = "00:00:00:00:00:02"
 
 
-SENDER_PORT   = [0]
+SENDER_PORT = [0]
 RECEIVER_PORT = [1]
-SHAPING_RATE_MBPS = 1000 # in Mbps
+SHAPING_RATE_MBPS = 1000  # in Mbps
 
 
 class PortShapingSTL(StatelessTest):
@@ -33,7 +33,6 @@ class PortShapingSTL(StatelessTest):
     def setup_subparser(cls, parser: ArgumentParser) -> None:
         parser.add_argument("--duration", type=float, help="Test duration", default=10)
 
-
     # The entrypoint of a test
     def start(self, args: dict) -> None:
 
@@ -42,9 +41,8 @@ class PortShapingSTL(StatelessTest):
         stream = STLStream(packet=STLPktBuilder(pkt=pkt, vm=[]), mode=STLTXCont())
         self.client.add_streams(stream, ports=[0])
 
-
         logging.info(
-                "Starting traffic, duration: %d sec", args.duration,
+            "Starting traffic, duration: %d sec", args.duration,
         )
 
         # Start sending traffic
@@ -55,16 +53,16 @@ class PortShapingSTL(StatelessTest):
 
         # Get statistics for TX and RX ports
         stats = self.client.get_stats()
-        rx_rate_mbps = stats[1]['rx_bps'] / (10**6)
-        assert (SHAPING_RATE_MBPS*0.95 < rx_rate_mbps < SHAPING_RATE_MBPS), \
-        "The measured RX rate is not close to the port shaping rate"
-
+        rx_rate_mbps = stats[1]["rx_bps"] / (10 ** 6)
+        assert (
+            SHAPING_RATE_MBPS * 0.95 < rx_rate_mbps < SHAPING_RATE_MBPS
+        ), "The measured RX rate is not close to the port shaping rate"
 
         readable_stats_0 = get_readable_port_stats(stats[0])
         readable_stats_1 = get_readable_port_stats(stats[1])
 
-        print ("\n Statistics for TX port: \n")
-        print (readable_stats_0)
+        print("\n Statistics for TX port: \n")
+        print(readable_stats_0)
 
-        print ("\n Statistics for RX port: \n")
-        print (readable_stats_1)
+        print("\n Statistics for RX port: \n")
+        print(readable_stats_1)
